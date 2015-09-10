4377e15990a3409c33789eaf25b003df1a981318
RVerhoef <r.verhoef@hotmail.nl>
2015-09-10 22:03:03 +0200

    Added Following Camera

---
+++ Assets/Scripts/UI/FollowPlayer.cs
@@ -0,0 +1,29 @@
+﻿//Written by Rob Verhoef
+using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Transform _player;
+    //private float _followSpeed = 8;
+    private Vector3 _offset;
+
+	void Awake ()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        _offset = new Vector3(transform.position.x, transform.position.y, -10);
+	}
+	
+	void Update ()
+    {
+        //float step = _followSpeed * Time.deltaTime;
+
+        if (_player != null)
+        {
+            //transform.position = Vector3.MoveTowards (transform.position, new Vector3(_player.transform.position.x, _player.transform.position.y, -10), step);
+            transform.position = _player.position + _offset;
+        }
+
+	}
+
+}
